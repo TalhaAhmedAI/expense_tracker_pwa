adf3eac9a3dbc33518f6d82c29a1091d6350a428
tester <aiforeveryoneai@gmail.com>
2020-07-27 12:48:26 +0500

    initial commit

---
+++ src/components/balance.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Balance: React.SFC = () => {
+  return <h3>YOUR BALANCE</h3>;
+};
+
+export default Balance;
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header: React.SFC = () => {
+  return <h1>Expense Tracker</h1>;
+};
+
+export default Header;
